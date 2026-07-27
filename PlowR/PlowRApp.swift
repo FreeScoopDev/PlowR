4eee42745ac7ac34c9dd09f637c3ed21a15ff01a
Joe <josephx10@hotmail.com>
2026-07-27 16:34:27 -0400

    Initial Commit

---
+++ PlowR/PlowRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlowRApp.swift
+//  PlowR
+//
+//  Created by Joe Amanatidis on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PlowRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
